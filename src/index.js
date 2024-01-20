1bd4ef7bc24987962f2571ee604aecca10db7cec
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-01-20 16:00:52 -0500

    Create basic form with dialog

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 'use strict';
-
+// https://api.weatherapi.com/v1/current.json?key=04751a96d37d46faac2201108242001&q=Paris
+// https://api.weatherapi.com/v1/forecast.json?key=04751a96d37d46faac2201108242001&q=Paris&days=3
+// https://api.weatherapi.com/v1/current.json?key=04751a96d37d46faac2201108242001&q=auto:ip
+// https://api.weatherapi.com/v1/history.json?key=04751a96d37d46faac2201108242001&q=auto:ip&dt=2023-08-10
 import './style.css';
 
